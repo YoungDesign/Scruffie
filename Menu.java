e1a6da33cabffa33b9c68fe1e4c977cf3f56d384
Williamsapplicatie <chouten8440@student.landstede.nl>
2017-09-23 16:48:31 +0200

    Add files via upload
    Scruffie version 1.0.0 Build 1

---
+++ Menu.java
@@ -0,0 +1,44 @@
+package menu;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Menu extends BasicGameState {
+	
+	private Image gameMenu;
+
+	
+	@Override
+	public void init(GameContainer gc , StateBasedGame sbg)throws SlickException {
+		gameMenu = new Image("afbeeldingen/Scruffie.png");
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException {
+			
+		gameMenu.draw(0,0);
+			
+		}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	Made by 
+	
+	// Made By:  YoungDesigns & Williamsapplicatie 2017 © copyright
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
