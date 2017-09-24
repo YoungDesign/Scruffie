4cfac6c8357a46af6d0fdc8d355bf4ab8d72e9c7
C.Williams <dhr.cwilliams@gmail.com>
2017-09-24 23:39:32 +0200

    update game varibales

--- Menu.java
+++ Menu.java
@@ -17,30 +17,32 @@ import org.newdawn.slick.state.StateBasedGame;
 
 
 public class Menu extends BasicGameState {
+
+	private static  boolean spelIsGestart;
+	private static String speler;
 	
 	private Image goudenFrame;
 
 	
 	@Override
 	public void init(GameContainer gc , StateBasedGame sbg)throws SlickException {
+		// goudenframe in menu
 		goudenFrame = new Image("afbeeldingen/menu/goudenframe.png");
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException {
-			
 		goudenFrame.draw(0,0);
+		
 			
 		}
 																																										
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
-		// TODO Auto-generated method stub
 
 	}
 	
 	
-	// Made By:  YoungDesigns & Williamsapplicatie 2017 © copyright
 
 	@Override
 	public int getID() {
